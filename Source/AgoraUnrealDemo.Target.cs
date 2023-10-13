21846fb0a7e589e9cab2f5c49b6f7bbc16767431
rickagora <rick@agora.io>
2023-10-12 23:40:20 -0700

    Updated build settings for Mac and changed title text

--- Source/AgoraUnrealDemo.Target.cs
+++ Source/AgoraUnrealDemo.Target.cs
@@ -11,5 +11,15 @@ public class AgoraUnrealDemoTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 
 		ExtraModuleNames.AddRange( new string[] { "AgoraUnrealDemo" } );
+
+
+		if (Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			bOverrideBuildEnvironment = true;
+			// -Wno-unused-but-set-variable£ fix error "variable 'layerNames' set but not used"
+			//  -Wno-gcc-compat: gcc does not allow an atrribute in this position on a function declaration
+			// -Wno-reorder-ctor: fix error "field 'eventHandler' will be initialized after field 'mccUid'"
+			AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-gcc-compat -Wno-reorder-ctor";
+		}
 	}
 }
